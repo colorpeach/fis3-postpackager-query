fcf54e5e78a518f6e308a4e8c9c95701a89a41a4
colorpeach <514571352@qq.com>
2016-02-22 15:57:50 +0800

    fis自定义query

---
+++ README.md
@@ -0,0 +1,43 @@
+# fis3-postpackager-query
+
+给文件添加自定义query，默认是md5
+
+## 安装
+
+```
+npm install -g fis3-postpackager-query
+```
+
+## 使用方法
+
+### 配置
+
+```js
+// 设置占位符
+var query = '?v=' + Date.now();
+
+// 应用占位符
+fis.match('*', {
+    query: query
+});
+
+// 基本用法
+fis.match('::package', {
+  // 默认query为md5
+  postpackager : fis.plugin('query', {
+    placeholder: query // 这里传入占位符
+  })
+});
+
+// 自定义用法
+fis.match('::package', {
+  postpackager : fis.plugin('query', {
+    placeholder: query, // 这里传入占位符
+    // 不使用replace参数时，默认输出每个文件对应的md5作为query部分
+    // ret：打包信息，subpath：处理的资源路径，file：subpath对应的文件对象
+    replace: function (ret, subpath, file) {
+      return Math.random();
+    }
+  })
+});
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var path = require("path");
+
+module.exports = function(ret, conf, settings, opt){
+  if (!settings.placeholder) {
+    throw '缺少query占位参数';
+  }
+  
+  var placeholder = settings.placeholder.split("=");
+  var key = placeholder[0].substring(1); // 占位符的key
+  var reg = new RegExp('(\/[^\\?\\*\\|<>:"]+)\\?' + key + '=' + placeholder[1], 'mg'); // 匹配占位符的正则
+  var hasGenerate = {}; // 记录已经处理过的文件
+  var cb = settings.replace; // 自定义的替换函数
+
+  // 根据匹配到的路径，返回对应文件
+  var findFile = function(subpath) {
+    var file = ret.pkg[subpath] || ret.src[subpath];
+
+    if (!file) {
+      for (var k in ret.src) {
+        if (ret.src[k].release === subpath) {
+        return ret.src[k];
+        }
+      }
+    }
+
+    return file;
+  };
+
+  // 替换占位符
+  var replace = function(subpath, file) {
+    var content = file.getContent();
+
+    if (content.replace) {
+      file.setContent(
+        content.replace(reg, function (str, res) {
+          var resFile = findFile(res);
+
+          if (!resFile) return str;
+
+          if (!hasGenerate[res]) {
+            // 如果用户自定义了替换函数，就不使用md5
+            if (cb) {
+              replace(res, resFile);
+              hasGenerate[res] = cb(ret, res, resFile);
+            } else {
+              hasGenerate[res] = fis.util.md5(replace(res, resFile));
+            }
+          }
+
+          return res + '?' + key + '=' + hasGenerate[res];
+        })
+      );
+    }
+
+    return content;
+  }
+
+  fis.util.map(ret.src, function (subpath, file) {
+    replace(subpath, file);
+  });
+  
+  hasGenerate = null;
+
+};
