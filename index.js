d5ffb43acc7a9f16a30d5ca097f5e2e2e964fe77
rpdg <rpdg@users.noreply.github.com>
2018-08-24 10:59:45 +0800

    Update index.js
    加上了支持url和domain的修改
    来自 https://github.com/wangchun0020/fis3-postpackager-query-x

--- index.js
+++ index.js
@@ -1,79 +1,91 @@
-'use strict';
+"use strict";
 
 var path = require("path");
 
-module.exports = function(ret, conf, settings, opt){
-  if (!settings.placeholder) {
-    fis.log.error('fis3-postpackager-query: 缺少query占位参数');
-    return;
-  }
-  
-  var placeholder = settings.placeholder.split("=");
-  var key = placeholder[0].substring(1); // 占位符的key
-  var reg = new RegExp('(\/[^\\?\\*\\|<>:"]+)\\' + settings.placeholder, 'mg'); // 匹配占位符的正则
-  var hasGenerate = {}; // 记录已经处理过的文件
-  var cb = settings.replace; // 自定义的替换函数
-  var root = fis.project.getProjectPath();
+module.exports = function(ret, conf, settings, opt) {
+	if (!settings.placeholder) {
+		fis.log.error("fis3-postpackager-query: 缺少query占位参数");
+		return;
+	}
 
-  // 根据匹配到的路径，返回对应文件
-  var findFile = function(subpath) {
-    var file = ret.pkg[subpath] || ret.src[subpath];
+	var placeholder = settings.placeholder.split("=");
+	var key = placeholder[0].substring(1); // 占位符的key
+	var reg = new RegExp('(/[^\\?\\*\\|<>:"]+)\\' + settings.placeholder, "mg"); // 匹配占位符的正则
+	var hasGenerate = {}; // 记录已经处理过的文件
+	var cb = settings.replace; // 自定义的替换函数
+	var root = fis.project.getProjectPath();
 
-    if (!file) {
-      for (var k in ret.pkg) {
-        if (ret.pkg[k].release === subpath) {
-          return ret.pkg[k];
-        }
-      }
-      for (var k in ret.src) {
-        if (ret.src[k].release === subpath) {
-          return ret.src[k];
-        }
-      }
-    }
+	// 根据匹配到的路径，返回对应文件
+	var findFile = function(subpath) {
+		var file = ret.pkg[subpath] || ret.src[subpath];
 
-    return file;
-  };
+	var v_path = "";
+		if (!file) {
+			for (var k in ret.pkg) {
+				//存在url和release同时存在，url会覆盖release
+				v_path = ret.pkg[k].domain + (ret.pkg[k].url ? ret.pkg[k].url : ret.pkg[k].release);
+				if (v_path === subpath) {
+				  return ret.pkg[k];
+				}
+			  }
+			  for (var k in ret.src) {
+				v_path = ret.src[k].domain + (ret.src[k].url ? ret.src[k].url : ret.src[k].release);
+				if (v_path === subpath) {
+				  return ret.src[k];
+				}
+			  }
+		}
 
-  // 替换占位符
-  var replace = function(subpath, file) {
-    var content = file.getContent();
+		return file;
+	};
 
-    if (content.replace) {
-      file.setContent(
-        content = content.replace(reg, function (str, res) {
-          var resFile = findFile(res);
+	// 替换占位符
+	var replace = function(subpath, file) {
+		var content = file.getContent();
 
-          if (!resFile) return str;
+		if (content.replace) {
+			file.setContent(
+				(content = content.replace(reg, function(str, res) {
+					var resFile = findFile(res);
 
-          if (!hasGenerate[res]) {
-            // 如果用户自定义了替换函数，就不使用md5
-            if (cb) {
-              replace(res, resFile);
-              hasGenerate[res] = cb(ret, res, resFile);
-            } else {
-              hasGenerate[res] = fis.util.md5(replace(res, resFile));
-            }
-          }
+					if (!resFile) return str;
 
-          return res + '?' + key + '=' + hasGenerate[res];
-        })
-      );
-    }
+					if (!hasGenerate[res]) {
+						// 如果用户自定义了替换函数，就不使用md5
+						if (cb) {
+							replace(res, resFile);
+							hasGenerate[res] = cb(ret, res, resFile);
+						} else {
+							hasGenerate[res] = fis.util.md5(replace(res, resFile));
+						}
+					}
 
-    return content;
-  }
+					return res + "?" + key + "=" + hasGenerate[res];
+				}))
+			);
+		}
 
-  fis.util.map(ret.src, function (subpath, file) {
-    if(file.ext === '.ts'){
+		return content;
+	};
+
+
+
+	fis.util.map(ret.src, function(subpath, file) {
+		if(file.ext === '.ts' || file.ext === '.js'){
 			if(!ret.pkg[subpath]){
 				ret.pkg[subpath] = file;
-				file.map.uri = file.release + '?' + key + '=' + fis.util.md5(file.getContent() , 14);
-			}
-	}
-      replace(subpath, file);
-  });
-  
-  hasGenerate = null;
 
+				var qs ;
+				if (cb) {
+					qs = cb({}, {}, file);
+				} else {
+					qs = fis.util.md5(file.getContent() , 7);
+				}
+				file.map.uri = file.release + '?' + key + '=' + qs;
+			}
+		}
+		replace(subpath, file);
+	});
+
+	hasGenerate = null;
 };
